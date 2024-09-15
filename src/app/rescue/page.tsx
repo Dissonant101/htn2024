3f32f920b0c1f2b9347dffd52b9167d7501751e7
Cindy Li <rin.mp4@gmail.com>
2024-09-14 23:55:30 -0400

    added logo and title to site, basic map to resc

--- src/app/rescue/page.tsx
+++ src/app/rescue/page.tsx
@@ -1,6 +1,16 @@
 "use client";
+import MyMap from "@/components/MyMap";
 import MyNavbar from "@/components/MyNavbar";
 
 export default function Rescue() {
-  return <MyNavbar />;
+    return (
+        <div className="flex flex-col w-screen h-screen">
+            <div className="flex-shrink-0">
+                <MyNavbar />
+            </div>
+            <div className="flex-grow">
+                <MyMap />
+            </div>
+        </div>
+    );
 }
